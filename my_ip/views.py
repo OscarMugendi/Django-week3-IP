c64b44327e33f596f7b3f1d1410c43647bcf2aac
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-19 17:09:33 +0300

    Create authentication system

--- my_ip/views.py
+++ my_ip/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import os
+import json
+
+from decouple import config, Csv
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from django.templatetags.static import static
+from django.http  import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.http  import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.defaulttags import register
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from .email import send_welcome_email
+
+import datetime as dt
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
