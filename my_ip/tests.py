b4439e2062d995fea21ae8bc5f36ff157cc2e6a1
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-24 00:47:52 +0300

    Added some tests for Profile and Project models.

--- my_ip/tests.py
+++ my_ip/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from . models import Profile, Project
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    
+    def setUp(self):
+        self.profile = Profile(id = 1)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile)) 
+
+
+
+class ProjectTestClass(TestCase):
+
+    def setUp(self):
+        self.project = Project(id = 1) 
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project)) 
